337bbd3a40d9177cd0009053337e90c82716fb13
CaioRian187 <caiorian246810@gmail.com>
2026-06-20 13:46:20 -0300

    Criando o método toEntityFromDto no ExameMapper

--- src/main/java/com/TrabalhoBD/clinica/mapper/ExameMapper.java
+++ src/main/java/com/TrabalhoBD/clinica/mapper/ExameMapper.java
@@ -2,6 +2,8 @@ package com.TrabalhoBD.clinica.mapper;
 
 import com.TrabalhoBD.clinica.dtos.ExameResponseDTO;
 import com.TrabalhoBD.clinica.models.Exame;
+import com.TrabalhoBD.clinica.models.Medico;
+import com.TrabalhoBD.clinica.models.Paciente;
 
 public abstract class ExameMapper {
 
@@ -15,6 +17,17 @@ public abstract class ExameMapper {
                 exame.getMedico().getNome(),
                 exame.getPaciente().getId(),
                 exame.getPaciente().getNome()
+        );
+    }
+
+    public static Exame toEntityFromDto(ExameResponseDTO dto, Medico medico, Paciente paciente){
+        return new Exame(
+                dto.id(),
+                dto.nome(),
+                dto.dataHora(),
+                dto.descricao(),
+                medico,
+                paciente
         );
     }
 }
