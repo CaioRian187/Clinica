ecfe8db9b3afd1acd244a85a9d9832be0545637c
CaioRian187 <caiorian246810@gmail.com>
2026-06-22 21:06:02 -0300

    Criando a exception costumizada para utilizar nas validações

---
+++ src/main/java/com/TrabalhoBD/clinica/exceptionHandler/exception/ValidacaoAgendamentoException.java
@@ -0,0 +1,8 @@
+package com.TrabalhoBD.clinica.exceptionHandler.exception;
+
+public class ValidacaoAgendamentoException extends RuntimeException{
+
+    public ValidacaoAgendamentoException(String msg) {
+        super(msg);
+    }
+}
